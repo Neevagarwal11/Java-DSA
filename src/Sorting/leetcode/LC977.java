cae57e0e62a3a6dcb6d283e98145c9b9bfef08e8
Neev Agarwal <agarwalneev2005@gmail.com>
2025-09-21 23:24:05 +0530

    LC977

--- src/Sorting/leetcode/LC977.java
+++ src/Sorting/leetcode/LC977.java
@@ -14,7 +14,11 @@ public class LC977 {
     static public int[] sortedSquares(int[] nums) {
 
         for(int i = 0 ; i<=nums.length-1 ; i++){
+
+
             nums[i] = nums[i]*nums[i];
+
+            
         }
         Arrays.sort(nums);
         return nums;
