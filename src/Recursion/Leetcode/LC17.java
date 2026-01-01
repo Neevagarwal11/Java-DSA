09d931fa97e34243cd00397e2e4d87e95ffbe40d
Neev Agarwal <agarwalneev2005@gmail.com>
2026-01-01 23:11:25 +0530

    maze problems

--- src/Recursion/Leetcode/LC17.java
+++ src/Recursion/Leetcode/LC17.java
@@ -5,7 +5,9 @@ class LC17 {
     public static void main(String[] args) {
 
         // phone("", "12");
-        System.out.println(phoneArrayList("", "12"));
+        // System.out.println(phoneArrayList("", "12"));
+
+        System.out.println(dice("", 4));
     }
 
 
@@ -50,5 +52,25 @@ class LC17 {
         return list;
     }
 
+
+    static ArrayList<String> dice(String p  , int target){
+
+        if(target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+         ArrayList<String> list = new ArrayList<>(); 
+
+        for(int i=1 ; i<=6 && i<=target ;i++){
+
+            list.addAll(dice(p +i , target - i));
+
+        }
+
+        return list;
+
+    }
 
 }
