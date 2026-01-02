953f55e6927d3500ee2762f64097db806061ee7e
Neev Agarwal <agarwalneev2005@gmail.com>
2026-01-02 23:26:58 +0530

    maze with obsticles

--- src/Recursion/maze.java
+++ src/Recursion/maze.java
@@ -3,7 +3,18 @@ class maze{
 
     public static void main(String[] args){
 
-        System.out.println(count(3,3));
+        // System.out.println(count(3,3));
+
+        // path("", 3, 3);
+
+
+        boolean[][] board = {
+            {true,true,true},
+            {true,false,true},
+            {true,true,true},
+        };
+        pathObsticles("", board, 0, 0);
+
     }
 
     static int count(int r , int c){
@@ -15,8 +26,44 @@ class maze{
         int right = count(r , c-1);
         
         return left + right;
-
     }
 
+    static void path(String p , int r , int c){
+
+        if(r ==1 && c==1){
+            System.out.println(p);
+            return;
+        }
+
+        if(r >1){
+            path(p + 'D',  r-1 , c);
+        }
+        if(c>1){
+            path(p + 'R' , r  ,c-1 );
+        }
+    }
+    
+    
+    // Maze with obsticles
+
+    static void pathObsticles(String p , boolean[][] maze ,int r , int c){
+
+        if(r ==maze.length-1 && c==maze[0].length -1){
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[r][c]){
+            return;
+        }
+
+        if(r < maze.length-1){
+            pathObsticles(p + 'D',maze,  r+1 , c);
+        }
+        if(c < maze[0].length-1){
+            pathObsticles(p + 'R', maze , r  ,c+1 );
+        }
+    }
+    
 
 }
