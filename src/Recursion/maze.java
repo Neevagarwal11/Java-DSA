d0c7e845369e361d3ebc59142dd22669f202823a
Neev Agarwal <agarwalneev2005@gmail.com>
2026-01-05 23:02:45 +0530

    Backtracking Started

--- src/Recursion/maze.java
+++ src/Recursion/maze.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 class maze{
 
     public static void main(String[] args){
@@ -13,7 +13,10 @@ class maze{
             {true,false,true},
             {true,true,true},
         };
-        pathObsticles("", board, 0, 0);
+        // pathObsticles("", board, 0, 0);
+
+        int[][] path =new int[board.length][board[0].length];
+        allPathBack("", board, 0, 0, path, 0);
 
     }
 
@@ -65,5 +68,46 @@ class maze{
         }
     }
     
+
+    static void allPathBack(String p , boolean[][] maze , int r , int c ,int[][] path , int step){
+
+        if(r == maze.length - 1 && c == maze[0].length -1){
+            path[r][c]=step;
+            for(int[] arr :path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+        if(!maze[r][c]){
+            return;
+        }
+
+        maze[r][c]=false;
+        path[r][c] =step;
+
+        if(r < maze.length-1){
+            allPathBack(p + 'D', maze, r+1, c ,path , step+1);
+        }
+
+        if(c < maze[0].length-1){
+            allPathBack(p + 'R', maze, r, c+1 ,path,step+1);
+        }
+
+        if(r>0){
+            allPathBack(p + 'U', maze, r-1, c, path, step+1);
+        }
+
+        if(c>0){
+            allPathBack(p + 'L', maze, r, c-1, path, step+1);
+        }
+
+
+        maze[r][c] =true;
+        path[r][c] =0;
+
+
+    }
 
 }
