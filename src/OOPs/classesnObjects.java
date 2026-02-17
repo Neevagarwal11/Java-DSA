dd1cb511ebda4f5f982871538674092dd94be4db
Neev Agarwal <agarwalneev2005@gmail.com>
2026-02-17 22:21:17 +0530

    OOPs tut2 completed

--- src/OOPs/classesnObjects.java
+++ src/OOPs/classesnObjects.java
@@ -1,4 +1,4 @@
-package OOPs;
+ package OOPs;
 
 import java.util.Arrays;
 
@@ -6,9 +6,28 @@ public class classesnObjects {
 
     // 1. It is also called used defined data structure
     // 2. It's a name group of properties and functions 
-    // 3. Class is a logical construct where as object is the physical construct.  
+    // 3. Class is a logical construct where as object is the physical construct. 
+    
+    
+
+    // static variable
+    static int a= 4;
+    static int b;
+
+    static{
+        System.out.println("Hello from Staitc block");
+        b = 4*5;
+    }
+
+
 
     public static void main(String[] args) {
+
+        // Accessing & Printing the staic variables
+        classesnObjects obj = new classesnObjects();
+        System.out.println(classesnObjects.a + " "  + classesnObjects.b);
+
+
         
         // Declaring the student 
     Student neev = new Student();  //Dynamically allocates memory and return a ref to the object stored in heap memory.
@@ -20,6 +39,16 @@ public class classesnObjects {
     // System.out.println(neev.name);
 
         neev.greeting();   
+
+
+        // Wrapper Class 
+        // Interger class =45;
+
+        final int INCREASE = 45;    //using the final keyword the variable is now fixed and is not allowed to change the value in it.
+
+        final Student neev2 = new Student();
+        neev2.name = "Neev Agarwal";    // final keyword does not change the value of primitive datatype like int,String,float but for objects it can change the value within the obj but the obj's structure itself cannot be changed.
+        
 
     }
 
@@ -33,13 +62,11 @@ public class classesnObjects {
         void greeting(){
             System.out.println("Hello my name is " + this.name );
         }
-
         Student(){
             this.roll = 21;
             this.name = "Neev";
             this.marks = 89.0f;
         }
-
         // Student neev = new Student(21 , "Neev" , 89.0f);     this will create a new obj named neev with the values of the passed arguments.
         Student(int roll , String name , float marks){
             this.roll = roll;
@@ -48,7 +75,22 @@ public class classesnObjects {
 
         }
 
+    }
+
+
+    static void fun(){
+
+        // greeting()  Error---> As in static function a non-static function cannot be called directly
+
+        classesnObjects obj = new classesnObjects();
+        obj.greeting();     // An non-static function can be called by declaring and object and calling it through it 
 
     }
+
+    void greeting(){
+
+    }
+
+
     
 }
